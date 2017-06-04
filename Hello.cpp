56a9755af0ec2aea70ab3ba0170591f76abb5c32
Jonne Valola <jonnev@gmail.com>
2017-06-05 01:53:07 +0300

    2-bit hires mode added

--- Hello.cpp
+++ Hello.cpp
@@ -4,9 +4,14 @@ Pokitto::Core game;
 
 int main () {
 game.begin();
+game.display.loadRGBPalette(paletteCGA);
+game.display.setFont(fontAdventurer);
+game.display.persistence = true;
 while (game.isRunning()) {
-    if (game.update()) {
 
+    if (game.update()) {
+        game.display.color=random(2)+1;
+        game.display.setCursor(random(game.display.width),random(game.display.height));
         game.display.print("Hello World!");
     }
 
