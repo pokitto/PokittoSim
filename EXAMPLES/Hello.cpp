82f6e0c51a4b3169b171d9858f8d9735038bc6f9
Jonne Valola <jonnev@gmail.com>
2017-07-17 11:50:47 +0300

    Different demos moved to EXAMPLES subfolder

---
+++ EXAMPLES/Hello.cpp
@@ -0,0 +1,18 @@
+#include "Pokitto.h"
+
+Pokitto::Core game;
+
+
+int main () {
+game.begin();
+
+while (game.isRunning()) {
+
+    if (game.update()) {
+        game.display.print("Hello World!");
+    }
+
+}
+
+return 1;
+}
