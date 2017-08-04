0e79b09a14977b5c23b6503f2dfc2a0141093915
Stefan Sauer <ensonic@google.com>
2017-08-04 20:32:31 +0200

    Fix crash when initializing the audio.
    When we deref a pointer, we need to use the native platform pointer size.
    The previous code was trunctating the pointer and then reading from taht location.
    Fixes #14

--- POKITTO_CORE/PokittoFakeavr.h
+++ POKITTO_CORE/PokittoFakeavr.h
@@ -72,7 +72,7 @@ typedef uint8_t byte;
 #define PROGMEM
     #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
     #ifndef WIN32
-    #define pgm_read_word(addr) (*(const unsigned short *)(addr))
+    #define pgm_read_word(addr) (*(addr))
     #else
     #define pgm_read_word(addr) (*(const unsigned int *)(addr))
     #endif // WIN32
